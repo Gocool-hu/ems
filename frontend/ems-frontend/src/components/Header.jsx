20f8c8025d609a41656a51433f328cb35882343f
Gokul <ramgokul785@gmail.com>
2025-11-14 11:43:16 +0530

    first commit

---
+++ frontend/ems-frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <a className="navbar-brand" href="https://www.ems.net">Employee Management System</a>
+                
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default Header
